e33c3af2fc6634a875e993f44abde2b63899e023
Stepan Petrov <jakeepping11@gmail.com>
2023-12-24 21:21:44 +0400

    fix formatting

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,27 +1,47 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
-let config = process.config.weather || require('./config.json');
+let config = process.config.weather || require("./config.json");
 
 app.use(express.json());
 
 // CORS handling middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', config.domain);
-  res.header('Access-Control-Allow-Origin', config.domainLocal);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  const allowedOrigins = [config.domain, config.domainLocal];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
+app.use(
+  cors({
+    origin: [config.domainLocal, config.domain],
+  })
+);
+
 // Route to access Tomorrow API
-app.get('/get-weather', async (req, res) => {
+app.get("/get-weather", async (req, res) => {
   try {
-    const tomorrowAPIResponse = await axios.get(`https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey=` + config.tomorrowKey);
+    const tomorrowAPIResponse = await axios.get(
+      `https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey=` +
+        config.tomorrowKey
+    );
     res.json(tomorrowAPIResponse.data);
   } catch (error) {
-    console.log(error)
-    res.status(400).json({ error: 'Failed to fetch data from the Tomorrow API' });
+    console.log(error);
+    res
+      .status(400)
+      .json({ error: "Failed to fetch data from the Tomorrow API" });
   }
 });
 
