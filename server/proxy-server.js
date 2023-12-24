d2918ad260e9d0b6ac89f52af1d512e6702658d4
Stepan Petrov <jakeepping11@gmail.com>
2023-12-24 21:29:40 +0400

    refactoring

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   const allowedOrigins = [config.domain, config.domainLocal];
 
-  const origin = req.headers.origin;
+  const origin = req.headers?.origin;
 
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
